be902b3371d1387995d6e61178181db167b63e02
HaMZa <rhirha.hamza@gmail.com>
2020-05-05 18:31:54 +0000

    ft_strcmp now returns the diff instead of -1,0,1

--- libasm/ft_strcmp.s
+++ libasm/ft_strcmp.s
@@ -5,33 +5,21 @@ ft_strcmp:
 	push	rbx
 	push	rdx
 	mov		rbx, 0
-	jmp		.loop
+	mov		rdx, 0
 
 .loop:
 	mov		dl, BYTE [rdi + rbx]
-	cmp		dl, BYTE [rsi + rbx]	;cmp (s1[rbx], s2[rbx])
-	jg		.ret_g
-	jl		.ret_l
-	.null:
-		cmp		BYTE [rdi + rbx], 0
-		jz		.ret_e
-		inc		rbx
-		jmp		.loop
+	cmp		dl, BYTE [rsi + rbx]
+	jnz		.diff
+	cmp		dl, 0
+	jz		.diff
+	inc		rbx
+	jmp		.loop
 
-.ret_g:
+.diff:
+	mov		al, BYTE [rdi + rbx]
+	mov		dl, BYTE [rsi + rbx]
+	sub		rax, rdx
 	pop		rdx
 	pop		rbx
-	mov		rax, 1
-	ret
-
-.ret_l:
-	pop		rdx
-	pop		rbx
-	mov		rax, -1
-	ret
-
-.ret_e:
-	pop		rdx
-	pop		rbx
-	mov		rax, 0
 	ret
